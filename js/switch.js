f4ec31afb1cf1fdf2a0d477645335bab0291e92f
laszlocs <laszlocss@gmail.com>
2024-10-31 22:27:36 +0100

    Delete js directory

--- js/switch.js
+++
@@ -1,1 +0,0 @@
-(function($j){switch_style={onReady:function(){this.switch_style_click();},switch_style_click:function(){$(".box").click(function(){var id=$(this).attr("id");$("#switch_style").attr("href","css/"+id+".css");});},};$j().ready(function(){switch_style.onReady();});})(jQuery);
