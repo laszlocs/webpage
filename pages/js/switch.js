ab5396878be62be96122932ee2f4bb6f6c24d3f7
laszlocs <laszlocss@gmail.com>
2024-10-31 22:21:17 +0100

    Add files via upload

---
+++ pages/js/switch.js
@@ -0,0 +1,1 @@
+(function($j){switch_style={onReady:function(){this.switch_style_click();},switch_style_click:function(){$(".box").click(function(){var id=$(this).attr("id");$("#switch_style").attr("href","css/"+id+".css");});},};$j().ready(function(){switch_style.onReady();});})(jQuery);
