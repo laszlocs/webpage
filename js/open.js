8c958062f528485db4420a3baacc30118158ca4f
laszlocs <laszlocss@gmail.com>
2024-10-31 22:29:31 +0100

    Add files via upload

---
+++ js/open.js
@@ -0,0 +1,15 @@
+let links = document.querySelectorAll(".links > li> a")
+let bodyId = document.querySelector("body").id
+
+for (let link of links) {
+    if (link.dataset.active == bodyId) {
+        link.classList.add("active")
+    }
+}
+
+const hamburger = document.querySelector("#hamburger");
+    const mainMenu = document.querySelector("#main-menu ul");
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("open");
+      mainMenu.classList.toggle("open");
+    });
